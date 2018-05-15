fdd1a2beb828c88fa822e6ae082a95f4d7877640
Jared Lutz and Matt Nardoni <jared+matt@>
2018-05-15 16:46:04 -0700

    fix compile errors

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BagService } from '../bag.service';
 import { Bag } from '../models/bag.model';
-import { Cart } from '../models/cart.model';
 import { Router } from '@angular/router';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { ActivatedRoute, Params } from '@angular/router';
@@ -15,29 +14,28 @@ import { Location } from '@angular/common';
 })
 export class CartComponent implements OnInit {
 
-  carts: FirebaseListObservable<any[]>;
+  bags: FirebaseListObservable<any[]>;
   currentRoute: string = this.router.url;
-  cartToDisplay;
-  cartId: string;
+  bagToDisplay;
+  bagId: string;
   constructor(private route: ActivatedRoute, private location: Location, private router: Router, private BagService: BagService) { }
 
-//refactor all of this to use the right observable
-  // ngOnInit() {
-  //   this.carts = this.BagService.getBags();
-  //   this.route.params.forEach((urlParameters) =>  {
-  //     this.bagId = urlParameters['id'];
-  //   });
-  //   this.BagService.getBagsById(this.bagId).subscribe(dataLastEmittedFromObserver => {
-  //     this.bagToDisplay = dataLastEmittedFromObserver;
-  //   })
-  // }
-  //
-  // submitBag(bagId){
-  //   this.BagService.updateCart(bagId);
-  // }
-  //
-  // cartRemove(bagToDelete){
-  //   this.BagService.deleteFromCart(bagToDelete);
+  ngOnInit() {
+    this.bags = this.BagService.getBags();
+    this.route.params.forEach((urlParameters) =>  {
+      this.bagId = urlParameters['id'];
+    });
+    this.BagService.getBagsById(this.bagId).subscribe(dataLastEmittedFromObserver => {
+      this.bagToDisplay = dataLastEmittedFromObserver;
+    })
+  }
+
+  submitBag(bagId){
+    this.BagService.updateCart(bagId);
+  }
+
+  cartRemove(bagToDelete){
+    this.BagService.deleteFromCart(bagToDelete);
   }
 
 }
