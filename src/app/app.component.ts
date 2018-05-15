f006c7678a6134ad4247ebd8775186a15c63fdf6
Joe McKinney and Robin Sola <joe@>
2018-05-15 13:45:37 -0700

    update project to fix authentication mishap.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,27 +11,7 @@ import * as firebase from "firebase";
 })
 export class AppComponent {
   title = 'The Bean Lounge';
-  private user;
-  private isLoggedIn: Boolean;
-  private userName: String;
 
-  constructor(public authService: AuthenticationService, private router: Router) {
-    this.authService.user.subscribe(user =>  {
-      if (user === null) {
-        this.isLoggedIn = false;
-      } else {
-        this.isLoggedIn = true;
-        this.userName = user.displayName;
-      }
-    });
-  }
-
-  login() {
-    this.authService.login();
-  }
-
-  logout() {
-    this.authService.logout();
-  }
+  constructor(public authService: AuthenticationService, private router: Router) {  }
 
 }
