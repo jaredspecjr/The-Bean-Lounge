409a627f1be1cf9923eca8969af7c0ab6d24b617
Joe McKinney and Robin Sola <joe@>
2018-05-15 15:37:42 -0700

    style main page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,29 +13,11 @@ import * as firebase from "firebase";
   providers: [BagService, AuthenticationService]
 })
 export class HomeComponent implements OnInit {
-  private user;
-  private isLoggedIn: Boolean;
-  private userName: String;
+
 
   bags : FirebaseListObservable<any[]>;
   currentRoute: string = this.router.url;
   constructor(public authService: AuthenticationService, private router: Router, private BagService: BagService) {
-    this.authService.user.subscribe(user =>  {
-      if (user === null) {
-        this.isLoggedIn = false;
-      } else {
-        this.isLoggedIn = true;
-        this.userName = user.displayName;
-      }
-    });
-  }
-
-  login() {
-    this.authService.login();
-  }
-
-  logout() {
-    this.authService.logout();
   }
 
   goToDetailPage(clickedBag){
@@ -44,8 +26,5 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.bags = this.BagService.getBags();
-  }
-  ngDoCheck() {
-    this.user = firebase.auth().currentUser;
   }
 }
