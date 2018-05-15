336e58dd02586ff62144c4e20d377cfc01bda128
Jared Lutz and Matt Nardoni <matt@>
2018-05-15 16:29:28 -0700

    add cart model, and template info for cart, comented out code that needs refactoring

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { BagService } from '../bag.service';
 import { Bag } from '../models/bag.model';
+import { Cart } from '../models/cart.model';
 import { Router } from '@angular/router';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
@@ -12,16 +15,29 @@ import { FirebaseListObservable } from 'angularfire2/database';
 })
 export class CartComponent implements OnInit {
 
-  bags: FirebaseListObservable<any[]>;
+  carts: FirebaseListObservable<any[]>;
   currentRoute: string = this.router.url;
-  constructor(private router: Router, private BagService: BagService) { }
+  cartToDisplay;
+  cartId: string;
+  constructor(private route: ActivatedRoute, private location: Location, private router: Router, private BagService: BagService) { }
 
-  ngOnInit() {
-    this.bags = this.BagService.getBags();
-  }
-
-  addItem(item){
-  
+//refactor all of this to use the right observable
+  // ngOnInit() {
+  //   this.carts = this.BagService.getBags();
+  //   this.route.params.forEach((urlParameters) =>  {
+  //     this.bagId = urlParameters['id'];
+  //   });
+  //   this.BagService.getBagsById(this.bagId).subscribe(dataLastEmittedFromObserver => {
+  //     this.bagToDisplay = dataLastEmittedFromObserver;
+  //   })
+  // }
+  //
+  // submitBag(bagId){
+  //   this.BagService.updateCart(bagId);
+  // }
+  //
+  // cartRemove(bagToDelete){
+  //   this.BagService.deleteFromCart(bagToDelete);
   }
 
 }
